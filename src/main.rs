cefb8f5347856517ff46f79731effded6bdf8874
rajasingh545 <rajasingh545@gmail.com>
2025-04-26 09:40:51 +0530

    trait partial order

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,46 @@
-use rust::booking::accommodation::Accommodation;
-use rust::booking::airbnb::AirBnB;
-use rust::booking::hotel::Hotel;
+use std::cmp::Ordering;
+
+struct Job {
+    salary: u32,
+    commute_time: u32,
+}
+
+impl PartialEq for Job {
+    fn eq(&self, other: &Self) -> bool {
+        self.salary == other.salary
+    }
+}
+
+impl Eq for Job {}
+
+impl PartialOrd for Job {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.salary == other.salary {
+            Some(Ordering::Equal)
+        } else if self.salary < other.salary {
+            Some(Ordering::Less)
+        } else if self.salary > other.salary {
+            Some(Ordering::Greater)
+        } else {
+            None
+        }
+    }
+}
 
 fn main() {
-    let mut hotel = Hotel::new("Grand Hotel");
-    let description = hotel.get_description();
-    println!("{}", description);
-    hotel.book("Jhon Deo", 3);
-    hotel.book("Jane Doe", 5);
-    hotel.book("Alice", 2);
+    let long_commute_job = Job {
+        salary: 10000,
+        commute_time: 2,
+    };
 
-    book_for_one_night(&mut hotel, "Bob");
-    println!("{:#?}", hotel);
+    let short_commute_job = Job {
+        salary: 75000,
+        commute_time: 1,
+    };
 
-    let mut airbnb = AirBnB::new("Alice");
-    let description = airbnb.get_description();
-    println!("{}", description);
-    airbnb.book("Bob", 2);
-    airbnb.book("Charlie", 4);
-    airbnb.book("Eve", 3);
-
-    book_for_one_night(&mut airbnb, "Dave");
-    println!("{:#?}", airbnb);
-
-    mix_and_match(&hotel, &airbnb);
-}
-
-fn book_for_one_night<T: Accommodation>(entity: &mut T, guest: &str) {
-    entity.book(guest, 1);
-    println!("{} booked for one night", entity.get_description());
-}
-
-fn mix_and_match<T: Accommodation, U: Accommodation>(entity1: &T, entity2: &U) {
-    println!(
-        "Mixing and matching {} and {}",
-        entity1.get_description(),
-        entity2.get_description()
-    );
+    println!("{}", long_commute_job > short_commute_job);
+    println!("{}", long_commute_job < short_commute_job);
+    println!("{}", long_commute_job == short_commute_job);
+    println!("{}", long_commute_job >= short_commute_job);
+    println!("{}", long_commute_job <= short_commute_job);
 }
